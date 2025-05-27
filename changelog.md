47822bfb81c89fa9b155d727d435752e5211fa6a
Peter Jin <peterj9h@gmail.com>
2025-05-27 16:15:21 -0700

    pause menu music slider

---
+++ changelog.md
@@ -0,0 +1,8 @@
+# 1.1.1
+- Add support for music slider in pause menu
+
+# 1.1.0
+- Add Android build
+
+# 1.0.0
+- Initial release
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,27 @@
 
 using namespace geode::prelude;
 
+// Main menu music slider
 #include <Geode/modify/OptionsLayer.hpp>
 class $modify(OptionsLayer) {
-	void musicSliderChanged(CCObject* p) {
+	void musicSliderChanged(CCObject* p0) {
 		float new_vol;
 		FMODAudioEngine *F;
 
-		new_vol = reinterpret_cast<SliderThumb*>(p)->getValue();
+		new_vol = reinterpret_cast<SliderThumb*>(p0)->getValue();
+		F = FMODAudioEngine::sharedEngine();
+		F->setBackgroundMusicVolume(new_vol);
+	}
+};
+
+// In-level music slider
+#include <Geode/modify/PauseLayer.hpp>
+class $modify(PauseLayer) {
+	void musicSliderChanged(CCObject* p0) {
+		float new_vol;
+		FMODAudioEngine *F;
+
+		new_vol = reinterpret_cast<SliderThumb*>(p0)->getValue();
 		F = FMODAudioEngine::sharedEngine();
 		F->setBackgroundMusicVolume(new_vol);
 	}
